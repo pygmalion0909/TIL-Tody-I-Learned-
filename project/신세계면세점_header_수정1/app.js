607d7b421389930d78b1a041924239cba2095f7e
sos4424 <peel0220@gmail.com>
2019-12-21 21:22:38 +0900

    191221_수정

--- project/신세계면세점_header_수정1/app.js
+++ project/신세계면세점_header_수정1/app.js
@@ -1,18 +1,15 @@
-// const $header = document.querySelector('header');
-
-// $header.addEventListener('mouseenter', (event)=>{
-//   console.log(event.target)
-//   if(event.target.classList.contains('mh_lists')){
-//     $header.classList.add('header_active');
-//   }
-// },true)
-
-// $header.addEventListener('mouseleave', () => {
-//   $header.classList.remove('header_active');
-// });
-
-
 const $header = document.querySelector('#header');
+const $sub_header = document.querySelector('.sub_header');
+
+window.addEventListener('scroll', () => {
+  if(window.pageYOffset > $header.offsetTop){
+    $header.classList.add("soai");
+    $sub_header.classList.add('noneE');
+  }else{
+    $header.classList.remove("soai");
+    $sub_header.classList.remove("noneE");
+  }
+})
 
 $header.addEventListener('mouseenter', (event) => {
 	if(event.target.classList.contains('h_lists')){
@@ -23,3 +20,4 @@ $header.addEventListener('mouseenter', (event) => {
 $header.addEventListener('mouseleave', (event) => {
 	event.currentTarget.classList.remove('on_switch');
 })
+
